5bd3e62912f59434539e158c33270d7a7725b28a
Juhrer <54405118+Juhrer@users.noreply.github.com>
2019-09-03 08:45:28 -0300

    Update Contato.java

--- Contato.java
+++ Contato.java
@@ -7,8 +7,34 @@ public class Contato {
 		this.sobrenome = sobrenome;
 		this.telefone = telefone;
 	}
-	public boolean equals(Contato outroContato) {
-		return this.nome == outroContato.nome && this.sobrenome == outroContato.sobrenome;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((sobrenome == null) ? 0 : sobrenome.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contato other = (Contato) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (sobrenome == null) {
+			if (other.sobrenome != null)
+				return false;
+		} else if (!sobrenome.equals(other.sobrenome))
+			return false;
+		return true;
 	}
 	public String formatacaoContato() {
 		return "\n" + nome + " " + sobrenome + " - " + telefone + "\n";
